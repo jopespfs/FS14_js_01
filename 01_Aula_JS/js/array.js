fb88fd2a8db3566078b2eb824a41297b196eb9b1
jopespfs <juaumpedro2@gmail.com>
2023-05-04 21:46:15 -0300

    arrays + functions

---
+++ 01_Aula_JS/js/array.js
@@ -0,0 +1,8 @@
+let frutas = ['banana'];
+
+// add item no array
+frutas.push('uva');
+console.log(frutas);
+//remover a primeira fruta
+frutas.shift();
+console.log(frutas);
---
+++ 01_Aula_JS/js/functions.js
@@ -0,0 +1,15 @@
+function soma(){
+    
+    let n1 = document.getElementById('n1').value
+    let n2 = document.getElementById('n2').value
+
+    console.log(n1, n2)
+
+    let resultado = parseInt(n1) + parseInt(n2)
+    console.log(resultado)
+
+    //ou pode ser usado
+    // let resultado = +n1 + +n2
+    
+    
+}
