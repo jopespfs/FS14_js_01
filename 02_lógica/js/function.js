2b671b7b2cb61709deb9d33213fe5c93057e9bf8
jopespfs <juaumpedro2@gmail.com>
2023-05-11 19:56:02 -0300

    aula 02

---
+++ 02_lógica/js/function.js
@@ -0,0 +1,14 @@
+function soma(n1, n2) {
+    let resultado = n1 + n2
+    console.log('Soma:', resultado)
+
+    return(resultado)
+}
+//soma (123, 678)
+
+function media(n1, n2){
+    let resultado = soma(n1, n2)/2
+    console.log('Média:', resultado)    
+}
+
+media (123, 678)
